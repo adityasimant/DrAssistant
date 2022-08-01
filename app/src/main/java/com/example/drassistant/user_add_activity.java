d899247fb0aa95673f3415f3b6ca4715496bd005
adityasimant <adityasimant88@gmail.com>
2022-08-01 14:48:24 +0530

    initial commit

--- app/src/main/java/com/example/drassistant/user_add_activity.java
+++ app/src/main/java/com/example/drassistant/user_add_activity.java
@@ -1,13 +1,55 @@
 package com.example.drassistant;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class user_add_activity extends AppCompatActivity {
+
+    EditText etDate;
+    DatePickerDialog.OnDateSetListener setListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_add);
+
+        etDate = findViewById(R.id.etDate);
+
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+
+        etDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                DatePickerDialog datePickerDialog =
+                        new DatePickerDialog(
+                                user_add_activity.this, new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+
+                                month = month+1;
+                                String date = day+"/"+month+"/"+year;
+                                etDate.setText(date);
+
+
+                            }
+                        },year,month,day);
+                datePickerDialog.show();
+
+
+
+            }
+        });
+
     }
 }
