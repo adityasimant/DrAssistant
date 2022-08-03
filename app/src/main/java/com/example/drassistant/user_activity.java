300e47a60761623e4c5935f2481a5be7b36e1316
adityasimant <adityasimant88@gmail.com>
2022-08-03 11:31:56 +0530

    initial commit

--- app/src/main/java/com/example/drassistant/user_activity.java
+++ app/src/main/java/com/example/drassistant/user_activity.java
@@ -1,13 +1,31 @@
 package com.example.drassistant;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class user_activity extends AppCompatActivity {
+
+    ImageView useradd;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+
+        useradd = findViewById(R.id.imgUserAdd);
+
+        useradd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(user_activity.this,user_add_activity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
